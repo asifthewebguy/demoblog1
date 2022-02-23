e47aecce881b6aa675f8b82ef6c8326f80e0af22
asifthewebguy <asifthewebguy@users.noreply.github.com>
2022-02-23 20:33:05 +0000

    deploy: 1c0b93e76724ce21be4f25690b38118cd0159b6d

--- _nuxt/static/1645644701/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdemoblog1\u002F_nuxt\u002Fstatic\u002F1645644701",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fdemoblog1\u002F",assetsPath:"\u002Fdemoblog1\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"370f978d"}}}}(null));
